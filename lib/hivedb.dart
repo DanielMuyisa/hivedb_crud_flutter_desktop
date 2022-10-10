4647887d81ef5d3e833aad52511543f0242ef194
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-10 23:54:06 +0100

    updates

---
+++ lib/hivedb.dart
@@ -0,0 +1,13 @@
+// // HIVE data base
+
+// import 'package:hive/hive.dart';
+
+// part 'hivedb.g.dart';
+
+// @HiveType(typeId: 1)
+// class Users extends HiveObject {
+//   @HiveField(0)
+//   late String authname;
+//   @HiveField(1)
+//   late String password;
+// }
